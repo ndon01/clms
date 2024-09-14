14fd174ed7229bc0ef80861e3e43e2ba283abe98
joshlo39 <83348928+joshlo39@users.noreply.github.com>
2024-09-13 20:11:07 -0400

    Question bank select boxv2

--- clms-angular/src/app/modules/admin/question-bank/components/question-bank-question-table/question-bank-question-table.component.ts
+++ clms-angular/src/app/modules/admin/question-bank/components/question-bank-question-table/question-bank-question-table.component.ts
@@ -1,7 +1,7 @@
-import {Component, Input, Output} from '@angular/core';
+import {Component, Input, OnInit, Output} from '@angular/core';
 import {AdminModule} from "@modules/admin/admin.module";
 import {NgForOf, NgIf} from "@angular/common";
-import {PrimeTemplate} from "primeng/api";
+import {FilterMatchMode, FilterService, PrimeTemplate, SelectItem} from "primeng/api";
 import {TableModule} from "primeng/table";
 import {QuestionBankQuestion} from "@modules/admin/question-bank/model/question-bank-question.model";
 import {FormsModule} from "@angular/forms";
@@ -13,12 +13,32 @@ import {TagModule} from "primeng/tag";
   templateUrl: './question-bank-question-table.component.html',
   styleUrl: './question-bank-question-table.component.css'
 })
-export class QuestionBankQuestionTableComponent {
+export class QuestionBankQuestionTableComponent implements OnInit{
   @Input() questions: QuestionBankQuestion[] = [];
   @Input() useCheckBoxes: boolean = true;
   @Input() checkBoxValues: QuestionBankQuestion[] = [];
   @Output() checkBoxValuesChange: QuestionBankQuestion[] = [];
+  matchModeOptions: SelectItem[] | undefined;
 
+  constructor(private filterService: FilterService){}
+  ngOnInit(){
+    const customFilterName = 'custom-category';
+    this.filterService.register(customFilterName, (value:any, filter:any): boolean => {
+      if (filter === undefined || filter === null || filter.trim() === '') {
+        return true;
+      }
+      if (value === undefined || value === null) {
+        return false;
+      }
+      return value.lowercase().toString() === filter.lowercase().toString();
+    }
+    )
+    this.matchModeOptions = [
+      { label: 'Custom Equals', value: customFilterName },
+      { label: 'Starts With', value: FilterMatchMode.STARTS_WITH },
+      { label: 'Contains', value: FilterMatchMode.CONTAINS }
+    ];
+  }
   onCheckBoxClick(question: QuestionBankQuestion) {
     if (this.checkBoxValues.includes(question)) {
       this.checkBoxValues = this.checkBoxValues.filter(q => q !== question);
@@ -27,4 +47,6 @@ export class QuestionBankQuestionTableComponent {
     }
     this.checkBoxValuesChange = this.checkBoxValues;
   }
+
+
 }
--- clms-api/src/main/java/com/clms/api/questionBank/models/QuestionBankCategory.java
+++ clms-api/src/main/java/com/clms/api/questionBank/models/QuestionBankCategory.java
@@ -14,12 +14,4 @@ public class QuestionBankCategory{
     private int id;
 
     private String categoryName;
-    @Column
-@OneToMany(fetch = FetchType.EAGER) // change to lazy for performance later; eager for testing
-    @JoinTable(
-            name = "question_bank_category_children",
-            joinColumns = @JoinColumn(name = "parent_id"),
-            inverseJoinColumns = @JoinColumn(name = "child_id")
-    )
-    private List<QuestionBankCategory> categories;
 }
