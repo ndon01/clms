828485b51acef0ea547f1775750215b93027e42b
SebastianAbarca <122310256+SebastianAbarca@users.noreply.github.com>
2024-10-08 16:45:18 -0400

    Added frontend components
    due dates

--- clms-angular/src/app/modules/assignments/assignments.module.ts
+++ clms-angular/src/app/modules/assignments/assignments.module.ts
@@ -15,14 +15,17 @@ import {OrderListModule} from "primeng/orderlist";
 import {
     QuestionEditorViewComponent
 } from "@modules/assignments/components/question-editor-view/question-editor-view.component";
+import {InputGroupModule} from "primeng/inputgroup";
+import {FormsModule} from "@angular/forms";
+import {CalendarModule} from "primeng/calendar";
 
 
 
 @NgModule({
   declarations: [AssignmentOverviewPageComponent, AssignmentEditPageComponent],
   exports: [AssignmentOverviewPageComponent, AssignmentEditPageComponent],
-    imports: [
-        CommonModule, AssignmentsRoutingModule, CoreModule, DatePipe, ButtonDirective, Button, CourseCardComponent, SkeletonModule, OrderListModule, QuestionEditorViewComponent
-    ]
+  imports: [
+    CommonModule, AssignmentsRoutingModule, CoreModule, DatePipe, ButtonDirective, Button, CourseCardComponent, SkeletonModule, OrderListModule, QuestionEditorViewComponent, InputGroupModule, FormsModule, CalendarModule
+  ]
 })
 export class AssignmentsModule { }
