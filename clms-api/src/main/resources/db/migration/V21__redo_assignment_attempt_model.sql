74e0fefc4d5279ccdb4f71294bd739f6ea99552f
nickdonahue <117882806+nickdonahue@users.noreply.github.com>
2024-10-16 22:15:18 -0400

    migration fix

--- clms-api/src/main/resources/db/migration/V21__redo_assignment_attempt_model.sql
+++ clms-api/src/main/resources/db/migration/V21__redo_assignment_attempt_model.sql
@@ -30,7 +30,7 @@ BEGIN
         (
             id               UUID DEFAULT gen_random_uuid() PRIMARY KEY,
             assignment_attempt_id UUID NOT NULL REFERENCES assignment_attempts(id),
-            assignment_question_id INTEGER NOT NULL REFERENCES assignment_questions(id)
+            assignment_question_id INTEGER NOT NULL REFERENCES questions(id)
         );
 
         -- Set the owner of both tables to 'admin'
@@ -38,3 +38,15 @@ BEGIN
         ALTER TABLE assignment_question_attempts OWNER TO admin;
     END IF;
 END $$;
+
+CREATE TABLE IF NOT EXISTS assignment_attempts (
+    id            UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    assignment_id INTEGER NOT NULL REFERENCES assignments(id),
+    user_id       INTEGER NOT NULL REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS assignment_question_attempts (
+    id               UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    assignment_attempt_id UUID NOT NULL REFERENCES assignment_attempts(id),
+    assignment_question_id INTEGER NOT NULL REFERENCES questions(id)
+);
