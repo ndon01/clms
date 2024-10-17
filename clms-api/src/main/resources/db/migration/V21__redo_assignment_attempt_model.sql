d8426fd98d782979ab0099d956750b99e41c1f5b
nickdonahue <117882806+nickdonahue@users.noreply.github.com>
2024-10-17 14:02:13 -0400

    fix

--- clms-api/src/main/resources/db/migration/V21__redo_assignment_attempt_model.sql
+++ clms-api/src/main/resources/db/migration/V21__redo_assignment_attempt_model.sql
@@ -16,25 +16,5 @@ BEGIN
 
         -- Rename the existing 'question_attempts' table to keep a backup
         ALTER TABLE IF EXISTS question_attempts RENAME TO question_attempts_backup;
-
-        -- Recreate the 'assignment_attempts' table with UUID primary key
-        CREATE TABLE assignment_attempts
-        (
-            id            UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-            assignment_id INTEGER NOT NULL REFERENCES assignments(id),
-            user_id       INTEGER NOT NULL REFERENCES users(id)
-        );
-
-        -- Recreate the 'assignment_question_attempts' table with foreign key reference to 'assignment_attempts'
-        CREATE TABLE assignment_question_attempts
-        (
-            id               UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-            assignment_attempt_id UUID NOT NULL REFERENCES assignment_attempts(id),
-            assignment_question_id INTEGER NOT NULL REFERENCES questions(id)
-        );
-
-        -- Set the owner of both tables to 'admin'
-        ALTER TABLE assignment_attempts OWNER TO admin;
-        ALTER TABLE assignment_question_attempts OWNER TO admin;
     END IF;
 END $$;
