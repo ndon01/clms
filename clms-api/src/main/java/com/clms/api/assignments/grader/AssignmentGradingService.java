c2a41150f46234dd00764e09713cf879692480fe
SebastianAbarca <122310256+SebastianAbarca@users.noreply.github.com>
2024-11-25 23:42:34 -0500

    Started on Category Reccomendations
    Started on Category Reccomendations
    
    Co-Authored-By: Nicholas Donahue <117882806+ndon01@users.noreply.github.com>

--- clms-api/src/main/java/com/clms/api/assignments/grader/AssignmentGradingService.java
+++ clms-api/src/main/java/com/clms/api/assignments/grader/AssignmentGradingService.java
@@ -1,5 +1,7 @@
 package com.clms.api.assignments.grader;
 
+import com.clms.api.adaptiveLearning.CategoryReccomendationData;
+import com.clms.api.adaptiveLearning.CategoryReccomendationDataRepository;
 import com.clms.api.assignments.AssignmentQuestion;
 import com.clms.api.assignments.AssignmentQuestionAnswer;
 import com.clms.api.assignments.AssignmentQuestionRepository;
@@ -9,7 +11,9 @@ import com.clms.api.assignments.attempts.models.AttemptQuestionAnswer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 @Service
@@ -17,12 +21,15 @@ import java.util.UUID;
 public class AssignmentGradingService {
     private final AssignmentAttemptRepository attemptRepository;
     private final AssignmentQuestionRepository questionRepository;
+    private final CategoryReccomendationDataRepository categoryReccomendationDataRepository;
 
     public void grade(AssignmentAttempt attempt){
         List<AttemptQuestionAnswer> questionAnswers = attempt.getAnswers();
         if (questionAnswers == null || questionAnswers.isEmpty()) {
             return;
         }
+        Map<Integer, CategoryReccomendationData> categoryReccomendationDataHashMap = new HashMap<>();
+
         for (AttemptQuestionAnswer questionAnswer: questionAnswers) {
             AssignmentQuestion question = questionRepository.findById(questionAnswer.getQuestionId()).orElseThrow();
             //compare the answer with the correct answer in questions table
@@ -44,7 +51,26 @@ public class AssignmentGradingService {
                 questionAnswer.setSelectedAnswerCorrect(false);
             }
 
+            if(question.getSourceQuestionBankQuestion() != null){
+                var categories = question.getSourceQuestionBankQuestion().getCategories();
+                categories.forEach(category -> {
+                    var data = categoryReccomendationDataHashMap.getOrDefault(category.getId(), categoryReccomendationDataRepository
+                            .findByCategoryAndUser(category, attempt.getUser())
+                            .orElse(CategoryReccomendationData
+                                    .builder()
+                                    .category(category)
+                                    .user(attempt.getUser()).build()));
+                    data.setCategoryFrequency(data.getCategoryFrequency() + 1);
+                    if(questionAnswer.isSelectedAnswerCorrect()){
+                        data.setCategoryScore(data.getCategoryScore() + 1);
+                    }
+
+                });
+
+            }
+
         }
+        categoryReccomendationDataRepository.saveAll(categoryReccomendationDataHashMap.values());
         attempt.setScorePercentage((double)(attempt.getAnswersCorrect() == null ? 0 : attempt.getAnswersCorrect())/questionAnswers.size()*100);
         attemptRepository.save(attempt);
     }
